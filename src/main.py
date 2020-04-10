7cde640eeac17f8f273483478c0e0d24371e931b
duydeja-vu <duybarca99@gmail.com>
2020-04-10 19:57:10 +0700

    Defined mainProcessing class

--- src/main.py
+++ src/main.py
@@ -10,16 +10,34 @@ from multiprocessing import Process, Queue
 import os
 
 
+class MainProcessing(MainWindow):
+    def __init__(self):
+        self.q = Queue()
+        self.GUI_process_done = None
+        self.ROS_process_done = None
 
+    def StartGUI(self):
+        app = QApplication(sys.argv)
+        main_window = MainWindow(self.q)
+        main_window.InitUI()
+        sys.exit(app.exec_())
 
-q = Queue()
+    def StartROS(self):
+        rospy.init_node('main', anonymous=True)
+        data = []
+        data_temp = []
+        while not rospy.is_shutdown():
+            if self.q.qsize() != 0:
+                data = self.q.get()
+                data_temp = data
+                #print(self.GUI_process_done)
+            RobotControl(data_temp)
+            
 
 
 
 def RobotControl(data):
     msg = Twist()
-
-
     vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     pid_publisher = rospy.Publisher('/pid', String, queue_size=10)
     if len(data) != 0:
@@ -29,49 +47,17 @@ def RobotControl(data):
         msg.angular.x = int(data[6][0])
         msg.angular.y = int(data[6][1])
         msg.angular.z = int(data[6][2])
-        
-        
+        print(msg)
+        vel_publisher.publish(msg)
 
-        
 
-def StartGUI():
-    app = QApplication(sys.argv)
-    main_window = MainWindow(q)
-    main_window.InitUI()
-    sys.exit(app.exec_())
-   
-
-def StartROS():
-    rospy.init_node('main', anonymous=True)
-    data = []
-    data_temp = []
-    while not rospy.is_shutdown():
-        if q.qsize() != 0:
-            data = q.get()
-            data_temp = data
-        RobotControl(data_temp)
-
+main_process = MainProcessing()
 
 def main():
-    p_1 = Process(target=StartGUI)
-    p_2 = Process(target=StartROS)
+    p_1 = Process(target=main_process.StartGUI)
+    p_2 = Process(target=main_process.StartROS)
     p_1.start()
     p_2.start()
-
-   
-
-
-
-    
-        
-    
-    
-    
-
-
-    
-    
-        
 
 
 if __name__ == "__main__":
