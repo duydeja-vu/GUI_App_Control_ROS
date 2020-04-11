ba5e3782a7bb34fca4dadf15248aa715a3e36ece
duydeja-vu <duybarca99@gmail.com>
2020-04-11 20:33:07 +0700

    Add StartSocket method. This method handles connection of the program

--- src/main.py
+++ src/main.py
@@ -15,11 +15,13 @@ class MainProcessing(MainWindow, ServerSocket):
         self.q = Queue()
         self.GUI_process_done = None
         self.ROS_process_done = None
+        self.remote_control = False
 
     def StartGUI(self):
         app = QApplication(sys.argv)
-        main_window = MainWindow(self.q)
-        main_window.InitUI()
+        if self.remote_control == False:
+            main_window = MainWindow(self.q)
+            main_window.InitUI()
         sys.exit(app.exec_())
 
     def StartROS(self):
@@ -35,14 +37,22 @@ class MainProcessing(MainWindow, ServerSocket):
                 #print(self.GUI_process_done)
             RobotControl(data_temp, vel_publisher, pid_publisher)
 
-            
-
+    
+    def StartSocket(self):
+        server_socket = ServerSocket()
+        server_socket.CreateSocket()
+        server_socket.IP = '127.0.0.1'
+        server_socket.PORT = 15555
+        server_socket.BindAddr()
+        print("Bind Success")
+        self.remote_control = True
+        print(self.remote_control)
+        client_fd, client_addr = server_socket.ListenConnection()
+        
 
 
 def RobotControl(data, vel_pub, pid_pub):
-    
     msg = Twist()
-    
     if len(data) != 0:
         msg.linear.x = int(data[1][0])
         msg.linear.y = int(data[1][1])
@@ -50,12 +60,12 @@ def RobotControl(data, vel_pub, pid_pub):
         msg.angular.x = int(data[2][0])
         msg.angular.y = int(data[2][1])
         msg.angular.z = int(data[2][2])
-        print(type(msg.linear.x))
-        print(msg.linear.y)
-        print(msg.linear.z)
-        print(msg.angular.x)
-        print(msg.angular.y)
-        print(msg.angular.z)
+        # print(type(msg.linear.x))
+        # print(msg.linear.y)
+        # print(msg.linear.z)
+        # print(msg.angular.x)
+        # print(msg.angular.y)
+        # print(msg.angular.z)
 
         # print(data[0])
         vel_pub.publish(msg)
@@ -67,8 +77,10 @@ main_process = MainProcessing()
 def main():
     p_1 = Process(target=main_process.StartGUI)
     p_2 = Process(target=main_process.StartROS)
+    p_3 = Process(target=main_process.StartSocket)
     p_1.start()
     p_2.start()
+    p_3.start()
 
 
 if __name__ == "__main__":
