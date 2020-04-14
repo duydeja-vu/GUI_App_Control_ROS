98011d2c638d9bf813f06d577b18681237fddfe4
duydeja-vu <duybarca99@gmail.com>
2020-04-14 12:48:31 +0700

    Created ClientSocket class

--- src/client_code/client.py
+++ src/client_code/client.py
@@ -2,7 +2,7 @@ import socket
 from multiprocessing import Process, Queue
 import sys
 
-class Client(object):
+class ClientSocket(object):
     def __init__(self):
         try:
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
@@ -10,9 +10,26 @@ class Client(object):
             print("Failed {0}".format(err))
             sys.exit()
     
-        self.SERVER_HOST = ''
-        self.SERVER_PORT = ''
+        self.SERVER_HOST = '127.0.0.1'
+        self.SERVER_PORT = 15555
+        self.BUFFSIZE = 1024
     
     def ConnectToServer(self):
         self.client_socket.connect((self.SERVER_HOST, self.SERVER_PORT))
+    
+    def SendData(self, send_data):
+        self.client_socket.send(send_data.encode('utf-8'))
+
+    def RecvData(self):
+        recv_data = self.client_socket.recv(self.BUFFSIZE)
+        return recv_data
+
+
+
+
+
+
+
         
+
+
